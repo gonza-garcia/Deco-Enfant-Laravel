d30bbf50620c4592a07538ef501a6fcaef99eb03
toiarnoldi <victoriaarnoldi@gmail.com>
2019-04-15 16:44:37 -0300

    validacion formulario registro / crear y guardar usuario en db.json

--- recursos/funciones.php
+++ recursos/funciones.php
@@ -1,3 +1,125 @@
 <?php
 
-?>
+function validarRegistro($datos){
+    $errores = [];
+    $datosFinales = []; //NO VEO QUE SE USE EN OTRO LADO.
+
+    //TRIMEO - me parece que al final no se usa porque se trimea todo adento de la funcion armarUsuario(). O quizás no lo entiendo bien. Pero lo comento y anda todo. 
+
+    // foreach ($datos as $posicion => $valor) {
+    //     $datosFinales[$posicion] = trim($valor);
+    // }
+
+
+    // NOMBRE
+    if (strlen($datos["nombre"]) == 0) {
+        $errores["nombre"] = "Campo obligatorio";
+    }
+
+    //TELEFONO - ver
+    //campo vacio
+    if (strlen($datos["telefono"]) == 0) {
+        $errores["telefono"] = "Campo obligatorio";
+    }
+    // ingresar solo numeros
+    elseif (is_numeric($datos["telefono"]) == false) {
+        $errores["telefono"] = "El campo solo acepta numeros";
+    }
+
+
+    // EMAIL
+    // campo vacio
+    if (strlen($datos["email"]) == 0) {
+        $errores["email"] = "Campo obligatorio";
+    }
+    // formato email valido
+    elseif (filter_var($datos["email"], FILTER_VALIDATE_EMAIL) == false) {
+        $errores["email"] = "Ingrese un email válido";
+    }
+    // email ya registrado
+    elseif(existeUsuario($datos["email"])){
+        $errores["email"] = "El email ingresado ya se encuentra registrado";
+    }
+
+    //PASSWORD
+    //campo vacio
+    if (strlen($datos["pass"]) == 0) {
+        $errores["pass"] = "Campo obligatorio";
+    }
+    //campo vacio de reescribir contraseña
+    elseif (strlen($datos["pass2"]) == 0) {
+        $errores["pass"] = "Por favor repita la contraseña";
+    }
+    //coincidencia en las dos contraseñas
+    elseif ($datos["pass"] !== $datos["pass2"]) {
+        $errores["pass"] = "Las contraseñas no coinciden";
+    }
+
+    return $errores;
+}
+
+// funcion para que retorne e ingrese en armarUsuarrio() el ultimo id+1
+function lastId(){
+    $json = file_get_contents("recursos/db.json");
+    $array = json_decode($json, true);
+  
+      if($json==""){
+        return $lastId=0;
+      }
+    $ultimoElemento = array_pop($array["usuarios"]);
+  
+    $lastId = $ultimoElemento["id"] + 1;
+    return $lastId;
+  }
+  
+
+// Armar array de usuario
+function armarUsuario()
+{
+    return [
+        "id" => lastId(), // resultado de lastId()
+        "nombre" => trim($_POST["nombre"]),
+        "telefono" => trim($_POST["telefono"]),
+        "email" => trim($_POST["email"]),
+        "password" => password_hash($_POST["pass"], PASSWORD_DEFAULT),
+    ];
+}
+
+//Guardar usuario nuevo en archivo JSON 
+function guardarUsuario($user)
+{
+    $json = file_get_contents("recursos/db.json");
+    $array = json_decode($json, true);
+
+    $array["usuarios"][] = $user;
+    $array = json_encode($array, JSON_PRETTY_PRINT);
+
+    file_put_contents("recursos/db.json", $array);
+}
+
+//buscar por email para comprobar que el usuario no este ya registrado
+function buscarPorEmail($email){
+    if(!file_exists("recursos/db.json")){
+        $usuarios = "";
+    }
+    else {
+        $usuarios = file_get_contents("recursos/db.json");
+    }
+
+    if($usuarios == ""){
+        return null;
+    }
+
+    $array = json_decode($usuarios, true);
+
+    foreach ($array["usuarios"] as $usuario) {
+        if ($email === $usuario["email"]) { 
+            return $usuario;
+        } // aca iría un else?
+        return null;
+    }
+}
+
+function existeUsuario($email){
+    return buscarPorEmail($email) !== null;
+}
--- registro.php
+++ registro.php
@@ -1,3 +1,48 @@
+<?php
+
+require_once "recursos/funciones.php";
+
+$errores = [];
+
+$nameOk = "";
+$emailOk = "";
+$telOk = "";
+
+if ($_POST) {
+  //validar los campos y devolver errores.
+
+  // var_dump(existeUsuario($_POST["email"]));
+
+  $errores = validarRegistro($_POST);
+  // var_dump($errores);
+
+  $nameOk = trim($_POST["nombre"]);
+  $emailOk = trim($_POST["email"]);
+  $telOk = trim($_POST["telefono"]);
+
+
+  if (empty($errores)) { 
+    // Si no hay errores
+      //Crear usuario
+      if(!existeUsuario($_POST["email"])){
+        $usuario = armarUsuario();
+        //Guardar usuario      
+        guardarUsuario($usuario);
+        //redireccionar el usuario a la pagina de exito.
+        header("Location:registradoExito.php"); //nombre de archivo inventado. no existe todavia.  
+        exit;
+      }
+
+      else{
+       return "el usuario ya existe";
+      }
+      //Guardar Imagen
+  }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 
@@ -33,26 +78,59 @@
 
 
   <!-- FORM DE REGISTRO------------------------------------------------------------------------------------------------------->
-  <form class="col-sm-10 offset-sm-1 col-md-6 offset-md-3 col-lg-4 offset-lg-4" action="index.html" method="post">
-    <div class="form-group">
-      <label for="nombre">Nombre</label>
-      <input class="form-control" id="nombre" type="text" name="nombre" value="" placeholder="Ingrese su nombre aqui..." required>
-    </div>
-    <div class="form-group">
-      <label for="telefono">Telefono</label>
-      <input class="form-control" id="telefono" type="tel" name="telefono" value="" placeholder="Ingrese su telefono aqui...">
-    </div>
+  <form class="col-sm-10 offset-sm-1 col-md-6 offset-md-3 col-lg-4 offset-lg-4" action="registro.php" method="POST" enctype="multipart/form-data">
+    <?php if (!isset($errores["nombre"])) : ?>
+      <div class="form-group">
+        <label for="nombre">Nombre</label>
+        <input class="form-control" id="nombre" type="text" name="nombre" value="<?= $nameOk ?>" placeholder="Ingrese su nombre aqui...">
+        <span class="small"></span>
+      </div>
+    <?php else : ?>
+      <div class="form-group">
+        <label for="nombre">Nombre</label>
+        <input class="form-control border border-danger" id="nombre" type="text" name="nombre" value="" placeholder="Ingrese su nombre aqui...">
+        <span class="small text-danger"><?= $errores["nombre"] ?></span>
+      </div>
+    <?php endif; ?>
+    <?php if (!isset($errores["telefono"])) : ?>
+      <div class="form-group">
+        <label for="telefono">Telefono</label>
+        <input class="form-control" id="telefono" type="text" name="telefono" value="<?= $telOk ?>" placeholder="Ingrese su telefono aqui...">
+        <span class="small"></span>
+      </div>
+    <?php else : ?>
+      <div class="form-group">
+        <label for="telefono">Telefono</label>
+        <input class="form-control border border-danger" id="telefono" type="tel" name="telefono" value="" placeholder="Ingrese su telefono aqui...">
+        <span class="small text-danger"><?= $errores["telefono"] ?></span>
+      </div>
+    <?php endif; ?>
+
+    <?php if (!isset($errores["email"])) : ?>
     <div class="form-group">
       <label for="email">Email</label>
-      <input class="form-control" id="email" type="email" name="email" value="" placeholder="Ingrese su email aqui..." required>
+      <input class="form-control" id="email" type="email" name="email" value="<?= $emailOk ?>" placeholder="Ingrese su email aqui...">
     </div>
+    <?php else : ?>
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input class="form-control border border-danger" id="email" type="email" name="email" value="" placeholder="Ingrese su email aqui...">
+      <span class="small text-danger"><?= $errores["email"] ?></span>
+    </div>
+    <?php endif; ?>
+
     <div class="form-group">
       <label for="pass">Contraseña</label>
-      <input class="form-control" id="pass" type="password" name="pass" value="" placeholder="Ingrese su Contraseña aqui..." required>
+      <input class="form-control" id="pass" type="password" name="pass" value="" placeholder="Ingrese su Contraseña aqui...">
     </div>
+
     <div class="form-group">
       <label for="pass2">Confirmar Contraseña</label>
-      <input class="form-control" id="pass2" type="password" name="pass2" value="" placeholder="Repita su Contraseña aqui..." required>
+      <input class="form-control " id="pass2" type="password" name="pass2" value="" placeholder="Repita su Contraseña aqui...">
+    </div>
+    <div class="form-group">
+      <label for="avatar">Imagen de Perfil
+        <input class="form-contro" id="avatar" type="file" id="avatar" name="avatar" value="" placeholder="">
     </div>
     <div class="form-group">
       <button type="btn submit" class="btn btn-outline-primary" name="Registrarme" value="Registrarme">Registrarme</button>
@@ -66,7 +144,7 @@
 
 
   <!-- FOOTER -------------------------------------------------------->
-<?php include("recursos/footer.php") ?>
+  <?php include("recursos/footer.php") ?>
 
 
 
