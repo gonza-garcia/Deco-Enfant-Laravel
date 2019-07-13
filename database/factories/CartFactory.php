846e5e5b6a8fe81e89d35b8ea3101508b8e2f233
toiarnoldi <victoriaarnoldi@gmail.com>
2019-07-13 20:23:51 -0300

    cambio en product y cart controller

--- database/factories/CartFactory.php
+++ database/factories/CartFactory.php
@@ -5,14 +5,19 @@
 use App\Cart;
 use Faker\Generator as Faker;
 $factory->define(Cart::class, function (Faker $faker) {
-    $path = public_path('img/products');
+  $files = File::files(public_path('img/products'));
+  $images_list = [];
+  foreach ($files as $file)
+  {
+      $images_list[] = './img/products/' . pathinfo($file)['basename'];
+  }
 
     return [
       'name'            => $faker->sentence(3),
       'short_desc'      => $faker->sentence(5),
       'long_desc'       => $faker->sentence(8),
       'price'           => $faker->randomFloat(2, 300, 4000),
-      'thumbnail'       => $faker->image($path, 480, 600,'cats', false),
+      'thumbnail'       => $faker->randomElement($images_list),
       'cant'            => $faker->numberBetween($min = 0, $max = 1000),
       'discount'        => $faker->randomFloat(2, 10, 40),
       'cart_number'     => $faker->numberBetween($min = 1000, $max = 15000),
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -24,132 +24,122 @@ Dèco Enfant - Detalle producto
 
 
 
-<div class="py-3 pl-0">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 mb-0 pl-0 mb-2">
-        <a class="menu-esc" href="/">Inicio</a> <span class="mx-2 mb-0">/</span>
-        <a class="menu-esc" href="/productos">Productos</a> <span class="mx-2 mb-0">/</span>
-        {{-- //falta agregar esto en el controlador para que busque el nombre de la categoria del producto. y sea link a la vista de todos lo productos de esa categoria. --}}
-        <a class="menu-esc" href="/productos/{{ $product->subcategory->category->id }}">{{ $product->subcategory->category->name }}</a> <span class="mx-2 mb-0">/</span>
 
-        <span class="text-black">{{ $product->name }}</span>
-      </div>
+<div class="container py-3">
+  <div class="row">
+    <div class="col-md-12 mb-0 pl-0 mb-2">
+      <a class="menu-esc" href="/">Inicio</a><span class="mx-2 mb-0">/</span>
+      <a class="menu-esc" href="/productos">Productos</a> <span class="mx-2 mb-0">/</span>
+      <a class="menu-esc" href="/productos/{{ $product->subcategory->category->id }}">{{ $product->subcategory->category->name }}</a><span class="mx-2 mb-0">/</span>
+      <span class="text-black">{{ $product->subcategory->name }}</span>
     </div>
   </div>
 </div>
 
-<div class="detalle-producto">
 
-  <div class="container pl-0">
-
+<div class="detalle-producto">  
+  <div class="container pl-0">    
     <div class="row">
 
-      <div class="col-md-6">
-        <div class="border rounded">
-          {{-- @dd($product->thumbnail) --}}
-          <img src="{{url($product->thumbnail)}}" alt="{{ $product->name }}" class="img-fluid rounded">
-        </div>
-        <div class="center slider">
-          <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
-          <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
-          <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
-          <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
-          <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
-          <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
-        </div>
-      </div>
-
-      <div class="col-md-6">
-
-        <h2 class="prod-title text-dark">{{ $product->name }}</h2>
-        <p>{{$product->short_desc}}</p>
-        <p class="mb-4 text-justify">{{ $product->long_desc }}</p>
-        <p class="prod-price"><strong class="text-secondary h4">$ {{ $product->price }}</strong></p>
-
-        <div class="mb-1 d-flex">
-          {{-- @foreach ($product as $prod)
-          <label for="option-sm" class="d-flex mr-3 mb-3">
-            <span class="d-inline-block mr-2 position-relative">
-              <input type="radio" id="option-sm" name="shop-sizes">
-            </span>
-            <span class="d-inline-block text-black">{{ $prod->size }}</span>
-          </label>
-          @endforeach --}}
-          <label for="option-sm" class="d-flex mr-3 mb-3">
-            <span class="d-inline-block mr-2 position-relative">
-              <input type="radio" id="option-sm" name="shop-sizes">
-            </span>
-            <span class="d-inline-block text-black">{{ $product->size->name }}</span>
-          </label>
-          <label for="option-md" class="d-flex mr-3 mb-3">
-            <span class="d-inline-block mr-2 position-relative">
-              <input type="radio" id="option-md" name="shop-sizes">
-            </span>
-            <span class="d-inline-block text-black">Medium</span>
-          </label>
-          <label for="option-lg" class="d-flex mr-3 mb-3">
-            <span class="d-inline-block mr-2 position-relative">
-              <input type="radio" id="option-lg" name="shop-sizes">
-            </span>
-            <span class="d-inline-block text-black">Large</span>
-          </label>
-          <label for="option-xl" class="d-flex mr-3 mb-3">
-            <span class="d-inline-block mr-2 position-relative">
-              <input type="radio" id="option-xl" name="shop-sizes">
-            </span>
-            <span class="d-inline-block text-black">Extra Large</span>
-          </label>
-        </div>
-
-        <!-- Product Configuration -->
-        <div class="product-configuration">
-
-          <!-- Product Color -->
-          <div class="product-color mb-3 ">
-            {{-- <span class="mb-10">Color</span> --}}
-            <div class="color-choose">
-              <div>
-                <input data-image="color1" type="radio" id="color1" name="color" value="color1" checked>
-                <label for="color1"><span></span></label>
-              </div>
-              <div>
-                <input data-image="color2" type="radio" id="color2" name="color" value="color2">
-                <label for="color2"><span></span></label>
-              </div>
-              <div>
-                <input data-image="color3" type="radio" id="color3" name="color" value="color3">
-                <label for="color3"><span></span></label>
-              </div>
+      {{-- COLUMNA IZQUIERDA - IMAGEN DEL PRODUCTO --}}
+        <div class="col-md-6">
+            <div>
+                <img src="{{url($product->thumbnail)}}" alt="{{ $product->name }}" class="img-fluid mx-auto d-block rounded">
             </div>
-          </div>
-
-          {{-- <form class="" action="{{$product->id}}/cart?qty=1" method="get"> --}}
-              <div class="mb-3 ml-1">
-                <div class="qty mt-1">
-                  <span class="qty-sp minus bg-dark">-</span>
-                  <input type="number" class="qty-in count" name="qty" value="1">
-                  <span class="qty-sp plus bg-dark">+</span>
-                </div>
-              </div>
-
-              <p><a href="{{$product->id}}/cart" class="btn-cart btn btn-sm height-auto px-3 py-2 float-left "><i class="mr-1 fas fa-shopping-cart"></i>
-                Agregar</a></p>
-
-                {{-- <button type="submit" class="mr-1 fas fa-shopping-cart height-auto px-3 py-2 float-left" name="qty" value=1></button> --}}
-          {{-- </form> --}}
-
-          </div>
-
         </div>
 
+      {{-- COLUMNA DERECHA - DETALLE DEL PRODUCTO Y SLICK DE IMG--}}
 
+        <div class="col-md-6">
+          <div class="d-flex flex-column align-items-start">
 
-      
-      </div>
+                <h2 class="prod-title text-dark">{{ $product->name }}</h2>
+                <p>{{$product->short_desc}}</p>
+                <p class="mb-4 text-justify">{{ $product->long_desc }}</p>
+                <p class="prod-price"><strong class="text-secondary h4">$ {{ $product->price }}</strong></p>
+
+                <div class="mb-1 d-flex">
+                    <label for="option-sm" class="d-flex mr-3 mb-3">
+                        <span class="d-inline-block mr-2 position-relative">
+                          <input type="radio" id="option-sm" name="shop-sizes">
+                        </span>
+                        <span class="d-inline-block text-black">{{ $product->size->name }}</span>
+                    </label>
+        
+                    <label for="option-md" class="d-flex mr-3 mb-3">
+                        <span class="d-inline-block mr-2 position-relative">
+                          <input type="radio" id="option-md" name="shop-sizes">
+                        </span>
+                        <span class="d-inline-block text-black">Medium</span>
+                    </label>
+        
+                    <label for="option-lg" class="d-flex mr-3 mb-3">
+                        <span class="d-inline-block mr-2 position-relative">
+                          <input type="radio" id="option-lg" name="shop-sizes">
+                        </span>
+                        <span class="d-inline-block text-black">Large</span>
+                    </label>
+        
+                    <label for="option-xl" class="d-flex mr-3 mb-3">
+                        <span class="d-inline-block mr-2 position-relative">
+                          <input type="radio" id="option-xl" name="shop-sizes">
+                        </span>
+                        <span class="d-inline-block text-black">Extra Large</span>
+                    </label>
+                </div>
+
+                <div class="product-configuration">
+                  <div class="product-color mb-3 ">
+                    <div class="color-choose">
+                      <div>
+                        <input data-image="color1" type="radio" id="color1" name="color" value="color1" checked>
+                        <label for="color1"><span></span></label>
+                      </div>                      
+                      <div>
+                        <input data-image="color2" type="radio" id="color2" name="color" value="color2">
+                        <label for="color2"><span></span></label>
+                      </div>                      
+                      <div>
+                        <input data-image="color3" type="radio" id="color3" name="color" value="color3">
+                        <label for="color3"><span></span></label>
+                      </div>                      
+                    </div>
+                  </div>
+                </div>
+
+                <form class="" action="{{ url('/addToCart') }}" method="post">
+                  {{ csrf_field() }}
+                  <div class="mb-3 ml-1">
+                    <div class="qty mt-1">
+                      <span class="qty-sp minus bg-dark">-</span>
+                      <input type="text" class="qty-in count" name="cant" value=1 min="1" max="{{ $product->stock }}">
+                      <span class="qty-sp plus bg-dark">+</span>
+                      <input type="hidden" name="prodId" value="{{ $product->id }}">
+                    </div>
+                  </div>
+                  
+                  <p>
+                    <button type="submit" class="btn-cart btn btn-sm height-auto px-3 py-2 float-left"> Agregar  <i class="mr-1 fas fa-shopping-cart"></i>
+                    </button>
+                  </p>
+
+                  </form>
+
+                  <div class="center slider ml-3">
+                      <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
+                      <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
+                      <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
+                      <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
+                      <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
+                      <div class="slider-item"><img src="{{url($product->thumbnail)}}" alt=""></div>
+                  </div>
+
+          </div>
+        </div>
+
     </div>
   </div>
-
-
-
-  @endsection
+</div>
+         
+    
+@endsection
